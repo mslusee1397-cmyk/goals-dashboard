039d78c4cca9a4b8afc71a293545ed22cbec58f0
mslusee1397-cmyk <ms.lusee1397@gmail.com>
2026-09-03 10:52:08 +0300

    Enable dedicated mobile interface

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import { AuthProvider } from './AuthContext'
 import './styles.css'
+import './mobile.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
